f239558eca4d41e6d403b0e19bb5a0d514340c87
Luk1tahh <lucasolg23@gmail.com>
2025-09-17 22:36:03 -0300

    arreglo de FunctionAdd

--- web/src/assets/components/cards/Agregar.jsx
+++ web/src/assets/components/cards/Agregar.jsx
@@ -3,7 +3,7 @@ import './agregar.css'
 
 const Agregar = () => {
   const [step, setStep] = useState(0) // 0: nada, 1: nombre, 2:cantidad
-  const [nomb, serNomb] = useState("")
+  const [nomb, setNomb] = useState("")
   const [cant, setCant] = useState("")
   const [items, setItems] = useState([])
 
@@ -21,21 +21,39 @@ const Agregar = () => {
   };
 
   return (
-    <div class=''>
-      { step === 0 && (
-        <button onClick={add} class='bg-red-500'> AÑADIR ELEMENTO </button>
-      ) }
+    <div class='justify-center'>
+      { step === 0 && ( <button onClick={add} class='bg-blue-600 rounded-3xl p-4 hover:bg-blue-500'> AÑADIR ELEMENTO </button> ) }
 
       { step === 1 && (
         <input 
           type="text"
           placeholder='Nombre...'
-          value={nomb}
-          onChange={ (e) => setNomb(e.target.value) }
-          onKeyDown={ (e) => e.key === 'Enter' && add() }
-          class='bg-red-400'
+          value={nomb} 
+          onChange={ (e) => setNomb(e.target.value) } // se actualiza cada que es escribe
+          onKeyDown={ (e) => e.key === 'Enter' && add() } // al tocar ENTER ejecuta la funcion
+          class='bg-blue-800 text-white rounded-3xl p-4'
         />
-      )}
+      ) }
+
+      { step === 2 && (
+        <input 
+          type="number"
+          placeholder='Cantidad...' 
+          value={cant}
+          onChange={ (e) => setCant(e.target.value) }
+          onKeyDown={ (e) => e.key === "Enter" && add() }
+          class='bg-blue-800 text-white rounded-3xl p-4'
+        />
+      ) }
+      
+      <div class='my-4'>
+        { items.map( (item,i) => (
+          <div key={i} class='bg-gray-400 font-black rounded-4xl p-4 my-2'>
+            <span> {item.nomb} </span>
+            <span class='font-semibold'> X {item.cant} </span>
+          </div>
+        ) ) }
+      </div>
     </div>
   )
 }
--- web/src/assets/components/home/Home.jsx
+++ web/src/assets/components/home/Home.jsx
@@ -4,7 +4,7 @@ import './home.css'
 
 const Home = () => {
   return (
-    <div class='p-96'>
+    <div class='bg-black p-96'>
       <Agregar />
     </div>
   )
